79cf253bb4795646e9866bb91ec302943226ccbb
Bread0288 <42908590+Bread0288@users.noreply.github.com>
2019-01-19 22:25:48 +0900

    Before use ViewPager&Fragment(backup)

--- app/src/main/java/com/example/taxi_carpool/MainActivity.java
+++ app/src/main/java/com/example/taxi_carpool/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.taxi_carpool;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
